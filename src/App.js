2aecaf8afa09691b4e5ce96f807cf09a89ebacdd
Jason Andersen <jrandersen@users.noreply.github.com>
2024-02-01 19:40:58 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,129 @@
+import { Heading, IconButton, VStack, useColorMode, useDisclosure, useToast, Link, Flex } from "@chakra-ui/react";
+import TaskList from './components/tasks';
+import AddTask from './components/AddTask';
+import { FaSun, FaMoon, FaGithub, FaLinkedin, FaInstagram, FaTwitter, FaFacebook } from 'react-icons/fa'
+import { useState, useEffect } from 'react'
+
+function App() {
+
+    const toast = useToast();
+    const [tasks, setTasks] = useState(
+        () => JSON.parse(localStorage.getItem('tasks')) || []
+    );
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+
+    function deleteTask(id){
+        const newTasks = tasks.filter((task) => {
+            return task.id !== id;
+        });
+        setTasks(newTasks);
+    }
+
+    function deleteTaskAll(){
+        setTasks([]);
+    }
+
+    function checkTask(id){
+        
+        const newTasksCheck = tasks.map((task, index, array) => {
+            if (task.id === id){
+               task.check = !task.check;
+            }
+            return task;
+        });
+
+        setTasks(newTasksCheck);
+    }
+
+    function updateTask(id, body, onClose){
+
+        const info = body.trim();
+
+        if (!info) {
+            toast({
+                title: 'Digite sua tarefa',
+                position: 'top',
+                status: 'warning',
+                duration: 2000,
+                isClosable: true,
+            });
+            
+            return;
+        }
+
+        const newTasksUpdate = tasks.map((task, index, array) => {
+            if (task.id === id){
+               task.body = body;
+               task.check = false
+            }
+            return task;
+        });
+
+        setTasks(newTasksUpdate);
+
+        onClose();
+    }
+
+    function addTask(task){
+        setTasks([...tasks, task]);
+    }
+
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return(
+        <VStack p={4} minH='100vh' pb={28}>
+            <IconButton 
+                icon={colorMode === 'light' ? <FaSun /> : <FaMoon />}
+                isRound='true'
+                size='md'
+                alignSelf='flex-end'
+                onClick={toggleColorMode}
+            />
+
+            <Heading
+                p='5'
+                fontWeight='extrabold'
+                size='xl'
+                bgGradient='linear(to-l, teal.300, blue.500)'
+                bgClip='text'
+            >
+                Todo List
+            </Heading>
+            <AddTask addTask={addTask} />
+            <TaskList tasks={tasks} updateTask={updateTask} deleteTask={deleteTask} deleteTaskAll={deleteTaskAll} checkTask={checkTask}/>
+            
+            <Flex position='absolute' bottom='5'>
+                <Link href='https://github.com/jrandersen' target='_blank' >
+                    <IconButton 
+                    icon={<FaGithub/>}
+                    isRound='true'
+                    size='md'
+                    m='1'
+                /> 
+                </Link>
+                <Link href='https://www.linkedin.com/in/jason--andersen/' target='_blank'>
+                    <IconButton 
+                    icon={<FaLinkedin/>}
+                    isRound='true'
+                    size='md'
+                    m='1'
+                /> 
+                </Link>
+                <Link href='https://twitter.com/jasonrandersen' target='_blank'>
+                    <IconButton 
+                    icon={<FaTwitter/>}
+                    isRound='true'
+                    size='md'
+                    m='1'
+                /> 
+                </Link>
+            </Flex>
+        </VStack>
+    );
+}
+
+export default App;
+
---
+++ src/components/DeleteTask.js
@@ -0,0 +1,85 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    Text,
+    useDisclosure,
+    IconButton
+  } from '@chakra-ui/react'
+  import { useState } from 'react';
+  import React from 'react';
+  import { FiTrash2 } from 'react-icons/fi'
+
+  
+
+  function DeleteAllTask({ deleteTaskAll }) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <>
+            <Button
+                colorScheme='gray'
+                px='8'
+                h='45'
+                color='gray.500'
+                mt='8'
+                onClick={onOpen}
+                >
+                Clear All Tasks
+            </Button>
+
+            <Modal isCentered isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent w='90%'>
+                <ModalHeader>
+                    This will delete all tasks, are you sure?
+                </ModalHeader>
+                <ModalFooter>
+                <Button mr={3} onClick={onClose}>No</Button>
+                <Button colorScheme='blue' onClick={() => deleteTaskAll()}>
+                    Yes
+                </Button>
+                </ModalFooter>
+            </ModalContent>
+            </Modal>
+        </>
+    )
+  }
+
+  function DeleteTask({ task, deleteTask}) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+  
+    return (
+      <>
+        <IconButton
+            icon={<FiTrash2 />}
+            isRound='true'
+            onClick={onOpen}
+        />
+
+        <Modal isCentered isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent w='90%'>
+            <ModalHeader>
+                Are you sure you want to Delete this task?
+            </ModalHeader>
+            <ModalBody>
+                <Text>{task.body}</Text>
+            </ModalBody>
+            <ModalFooter>
+              <Button mr={3} onClick={onClose}>No</Button>
+              <Button colorScheme='blue' onClick={() => deleteTask(task.id, onClose)}>
+                Yes
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </Modal>
+      </>
+    )
+  }
+
+export { DeleteTask, DeleteAllTask }
